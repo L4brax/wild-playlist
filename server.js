817ba0e2fcfd9f6fa46b201b116d8f31ec0a501f
L4brax <ant.hess2@gmail.com>
2020-01-28 14:56:19 +0100

    Cleaning code

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const express = require("express");
 const bp = require("body-parser");
 require("dotenv").config();
 
-//Database connection
-const sql = require("./db.js");
-
 const app = express();
 const port = 80;
 
@@ -15,14 +12,6 @@ app.use(bp.json());
 var playlists = require("./routes/playlists");
 
 app.use("/api/v1/playlists", playlists);
-
-sql.query("Select * from playlist", function(err, res) {
-  if (err) {
-    console.log("error: ", err);
-  } else {
-    console.log(res);
-  }
-});
 
 app.listen(port, function() {
   // eslint-disable-next-line no-console
